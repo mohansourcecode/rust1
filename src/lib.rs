fba3d18604dc360771b811937adc5b143685e58f
mohan <mohan14242@gmail.com>
2024-08-26 09:53:34 +0530

    mohan

---
+++ src/lib.rs
@@ -0,0 +1,4 @@
+/// A simple function to print a greeting message.
+pub fn greet() {
+    println!("Hello from lib.rs!");
+}
---
+++ src/main.rs
@@ -0,0 +1,6 @@
+use rust_project::greet;
+
+fn main() {
+    println!("Hello from main.rs!");
+    greet();
+}
